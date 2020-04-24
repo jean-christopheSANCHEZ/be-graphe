4558198b8dbf33a0800b751e805386a631f4cde1
007jcs <jeanchristophesanchez@laposte.net>
2020-04-24 11:24:42 +0200

    label ok et disktra presque ok

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,12 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import java.util.ArrayList;
+
+import org.insa.graphs.algorithm.utils.BinaryHeap;
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.model.Node;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -10,8 +17,60 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
-        // TODO:
+        
+        //déclaration
+        Graph graph = data.getGraph();
+        Node[] noeuds;
+        noeuds = new Node[graph.size()];
+        Arc[] arcs ;
+        arcs = new Arc[graph.size()];
+        Label[] labels ;
+        labels = new Label[graph.size()];
+        
+        BinaryHeap <Label> tas = new BinaryHeap<Label>();
+       
+        
+        //On initialise la source à 0 ainsi que les tableaux
+        
+        for(int i = 0; i < graph.size();i++) {
+        	noeuds[i] = graph.get(i);
+        	if(data.getOrigin()==graph.get(i)) {
+        		Label origine = new Label(graph.get(i),null,0);
+        		tas.insert(origine);
+        	}else {
+        		
+        		labels[i] = new Label(graph.get(i),null,Float.POSITIVE_INFINITY);
+        	}
+        }
+        
+        while(tas.isEmpty()) {
+        	Label current = tas.deleteMin();
+        	current.setMarque(true);
+        	for(Arc arcsSucessor : current.getSommetCourant().getSuccessors()) { //on parcourt tout les successeurs de current
+        		
+        		if(labels[arcsSucessor.getDestination().getId()].getMarque() == false) { //si y est pas marquée
+        			if(labels[arcsSucessor.getDestination().getId()].getCout() > current.getCout() + arcsSucessor.getLength()) {
+        				labels[arcsSucessor.getDestination().getId()].setCout(current.getCout() + arcsSucessor.getLength()); // on met à jour le cost de y
+        				if(tas.) {
+        					tas.remove(labels[arcsSucessor.getDestination().getId()]);
+        					tas.insert(labels[arcsSucessor.getDestination().getId()]);
+        				}
+        				else {
+        					tas.insert(labels[arcsSucessor.getDestination().getId()]);
+        				}
+        			}
+        				
+        			
+        		}
+        	}
+        }
+        
+        
+        
+        
+        
         return solution;
     }
+
 
 }
--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -12,15 +12,87 @@ import org.insa.graphs.model.Node;
 
 
 
-public class Label {
+public class Label implements Comparable<Label>{
 	private Node sommetCourant;
 	private Boolean marque;
 	private double cout;
 	private Arc pere;
 	
+	public Label(Node courant, Arc parent, double cout) {
+		this.sommetCourant = courant;
+		this.marque = false;
+		this.cout = Float.POSITIVE_INFINITY;
+		this.pere = parent;
+	}
 	
+	
+	public Node getSommetCourant() {
+		return sommetCourant;
+	}
+
+
+
+
+	public void setSommetCourant(Node sommetCourant) {
+		this.sommetCourant = sommetCourant;
+	}
+
+
+
+
+	public Boolean getMarque() {
+		return marque;
+	}
+
+
+
+
+	public void setMarque(Boolean marque) {
+		this.marque = marque;
+	}
+
+
+
+
+	public Arc getPere() {
+		return pere;
+	}
+
+
+
+
+	public void setPere(Arc pere) {
+		this.pere = pere;
+	}
+
+
+
+
+	public void setCout(double cout) {
+		this.cout = cout;
+	}
+
+
+
+
 	public double getCout() {
 		return cout;
+	}
+
+
+	@Override
+	public int compareTo(Label arg0) {
+		int result;
+		if (this.getCout() < arg0.getCout()) {
+			result = -1;
+		}
+		else if (this.getCout() == arg0.getCout()) {
+			result = 0;
+		}
+		else {
+			result = 1;
+		}
+		return result;
 	}
 	
 }
